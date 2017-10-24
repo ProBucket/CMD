9f355beaa8c18284ace009645d2123b45f682c7f
ProBucket <hasan.khan.pro@gmail.com>
2017-10-24 13:41:45 +0600

    Create ms-sql-server.sql

---
+++ MS-SQL/ms-sql-server.sql
@@ -0,0 +1,10 @@
+
+==================================================================
+// Create SQL statements to actually drop those FK relations 
+
+SELECT 
+    'ALTER TABLE [' +  OBJECT_SCHEMA_NAME(parent_object_id) +
+    '].[' + OBJECT_NAME(parent_object_id) + 
+    '] DROP CONSTRAINT [' + name + ']'
+FROM sys.foreign_keys
+WHERE referenced_object_id = object_id('TeamCutOffs')
