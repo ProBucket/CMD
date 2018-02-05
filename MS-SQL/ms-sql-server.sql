82660e96e4e56772fe6e55563ee6503d7041d6b4
ProBucket <hasan.khan.pro@gmail.com>
2018-02-05 11:52:02 +0600

    Update ms-sql-server.sql

--- MS-SQL/ms-sql-server.sql
+++ MS-SQL/ms-sql-server.sql
@@ -8,3 +8,6 @@ SELECT
     '] DROP CONSTRAINT [' + name + ']'
 FROM sys.foreign_keys
 WHERE referenced_object_id = object_id('TeamCutOffs')
+
+
+delete from StudentCaptures where Student_Id in ( select Id from Students where School_Id = 1500 and Year = 2018 )
